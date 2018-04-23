3859206e271e1453d7f8e3e591f79a4dac8e1546
beaucouplus <maxime@beaucouplus.com>
2018-04-23 14:42:50 +0200

    finished tictactoe

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,8 +1,10 @@
 require 'pry'
+require 'pry-byebug'
 
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
+FIRST_PLAYER = %w[player computer choose].sample
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [2, 5, 8],
                  [3, 6, 9], [3, 5, 7], [1, 4, 7], [1, 5, 9]]
 
@@ -51,8 +53,34 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def find_at_risk_square(brd, player)
+  at_risk = WINNING_LINES.select do |line|
+    brd.values_at(*line).sort == [INITIAL_MARKER, player, player]
+  end
+  return nil if at_risk.empty?
+  playable_position = 0
+  at_risk.first.each do |position|
+    playable_position = position if brd[position] == INITIAL_MARKER
+  end
+  playable_position
+end
+
+def center_not_played?(brd)
+  brd[5] == INITIAL_MARKER
+end
+
+def most_relevant_position(brd)
+  threat = find_at_risk_square(brd, PLAYER_MARKER)
+  opening = find_at_risk_square(brd, COMPUTER_MARKER)
+
+  return opening if (opening && threat) || (opening && !threat)
+  return threat if threat && !opening
+  return 5 if !opening && !threat && center_not_played?(brd)
+  empty_squares(brd).sample
+end
+
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = most_relevant_position(brd)
   brd[square] = COMPUTER_MARKER
 end
 
@@ -91,25 +119,47 @@ def joinor(array, separator = ",", ending = "or")
 end
 
 def display_score(score)
-  score.to_a.map{ |sub| sub.join(": ") }.join(", ")
+  score.to_a.map { |sub| sub.join(": ") }.join(", ")
 end
 
 score = { player: 0, computer: 0 }
-def update_score(score,winner = nil)
+def update_score(score, winner = nil)
   score[winner.downcase.to_sym] += 1
   score
 end
 
+def define_players_order
+  players = ["player", "computer"]
+  answer = ""
+  if FIRST_PLAYER == "choose"
+    loop do
+      prompt("Do you want to begin? (y/n")
+      answer = gets.chomp
+      return players if answer.downcase.start_with?("y")
+      return players.reverse if answer.downcase.start_with?("n")
+      prompt("Wrong answer. Please choose y or n")
+    end
+  else
+    FIRST_PLAYER == "player" ? players : players.reverse
+  end
+end
+
+def play(curr, brd)
+  curr == "player" ? player_places_piece!(brd) : computer_places_piece!(brd)
+end
+
+def alternate_player(current_player)
+  current_player == "player" ? "computer" : "player"
+end
+
 loop do
   board = initialize_board
+  current_player = define_players_order[0]
   loop do
     display_board(board)
-
-    player_places_piece!(board)
+    play(current_player, board)
     break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    current_player = alternate_player(current_player)
   end
 
   display_board(board)
@@ -124,7 +174,9 @@ loop do
   prompt "Score: #{display_score(score)}"
   prompt "Play again? (y or n)"
   answer = gets.chomp
-  break prompt("Game finished: #{display_score(score)}") if score[:player] >= 5 || score[:computer] >= 5
+  if score[:player] >= 5 || score[:computer] >= 5
+    break prompt("Game finished: #{display_score(score)}")
+  end
   break unless answer.downcase.start_with?("y")
 end
 prompt "Thanks for playing TicTacToe. Goodbye!"
